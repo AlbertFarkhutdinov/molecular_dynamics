d65f2fe078a361aa2d07f3012e8500f58d96b9ee
Albert Farhutdinov <farhutdinov.a@mxm.email>
2021-02-16 10:20:31 +0300

    first results on slow cooling

--- scripts/constants.py
+++ scripts/constants.py
@@ -4,5 +4,12 @@ import os
 BASE_DIRECTORY = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PATH_TO_CONFIG = os.path.join(BASE_DIRECTORY, 'config_files')
 PATH_TO_DATA = os.path.join(BASE_DIRECTORY, 'data')
+PATH_TO_PLOTS = os.path.join(BASE_DIRECTORY, 'plots')
 TEMPERATURE_MINIMUM = 1e-15
 IS_LOGGED = True
+
+
+if __name__ == '__main__':
+    print(BASE_DIRECTORY)
+    print(PATH_TO_CONFIG)
+    print(PATH_TO_DATA)
