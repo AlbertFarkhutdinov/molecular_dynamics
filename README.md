fc8313d1bb37a372fa91800e0f46e1220b22872e
Albert Farhutdinov <farhutdinov.a@mxm.email>
2021-04-30 09:25:06 +0300

    simulations with different heating velocities and initial temperatures

--- README.md
+++ README.md
@@ -48,4 +48,18 @@
  - система единиц Леннард-Джонса — уметь объяснять и снабдить 
    графиком потенциала;
  - число итераций — что это?;
- - готовиться к выступлениям основательнее!
+ - готовиться к выступлениям основательнее!   
+
+
+### Справочные данные:
+Liquid argon:
+_____________
+ - mass = 6.69E-26 kilograms
+ - distance = sigma = 0.341E-9 meters
+ - energy = epsilon = 1.65E-21 joules
+ - temperature = epsilon / k_B = 119.8 kelvin
+ - tau = sigma * sqrt(mass / epsilon) = 2.17E-12 seconds
+ - velocity = sigma / tau = 1.57E2 m/s
+ - force = epsilon/sigma = 4.85E-12 newtons
+ - pressure = epsilon / (sigma ^ 3) = 4.2E7 pascal = 4.2E2 atmospheres
+ - heating velocity = epsilon / k_B / tau = 0.55E14 K/s
--- scripts/main.py
+++ scripts/main.py
@@ -1,18 +1,3 @@
-"""
-Liquid argon:
-_____________
-mass = 6.69E-26 kilograms
-distance = sigma = 0.341E-9 meters
-energy = epsilon = 1.65E-21 joules
-temperature = epsilon / k_B = 119.8 kelvin
-tau = sigma * sqrt(mass / epsilon) = 2.17E-12 seconds
-velocity = sigma / tau = 1.57E2 m/s
-force = epsilon/sigma = 4.85E-12 newtons
-pressure = epsilon / (sigma ^ 3) = 4.2E7 pascal = 4.2E2 atmospheres
-heating velocity = epsilon / k_B / tau = 0.55E14 K/s
-
-"""
-
 from typing import List
 
 import numpy as np
@@ -44,7 +29,7 @@ if __name__ == '__main__':
 
     main(
         config_filenames=[
-            'cooling_slow.json',
+            'cooling_normal_0.3.json',
         ],
         is_with_isotherms=True,
     )
