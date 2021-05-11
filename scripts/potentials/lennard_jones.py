974d1255847ce1e03a7bfa1d8047d92418f6b8e9
Albert Farhutdinov <farhutdinov.a@mxm.email>
2021-05-11 10:58:15 +0300

    added log info for lennard_jones

--- scripts/potentials/lennard_jones.py
+++ scripts/potentials/lennard_jones.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from scripts.potentials.base_potential import BasePotential
+from scripts.log_config import logger_wraps, log_debug_info
 
 
 class LennardJones(BasePotential):
@@ -8,18 +9,32 @@ class LennardJones(BasePotential):
     def __repr__(self):
         return f'{self.__class__.__name__}(table_size = {self.table_size!r})'
 
+    @logger_wraps()
     def get_energies_and_forces(self):
         # forces - это величины -dU/rdr.
         # Сила получается умножением на вектор r_ij
         sigma_pow6, epsilon_x4 = self.sigma ** 6, self.epsilon * 4
+        log_debug_info(
+            'sigma_pow6, epsilon_x4, r_cut, table_size = '
+            f'{sigma_pow6}, {epsilon_x4}, {self.r_cut}, {self.table_size}'
+        )
+        log_debug_info(
+            f'r_ij = {self.distances.min()}, {self.distances.mean()}, '
+            f'{self.distances.max()}'
+        )
         energy_r_cut = epsilon_x4 * (
                 sigma_pow6 * sigma_pow6 / (self.r_cut ** 12)
                 - sigma_pow6 / (self.r_cut ** 6)
         )
+        log_debug_info(f'energy_r_cut = {energy_r_cut}')
         energies = epsilon_x4 * (
                 sigma_pow6 * sigma_pow6 / (self.distances ** 12)
                 - sigma_pow6 / (self.distances ** 6)
         ) - energy_r_cut
+        log_debug_info(
+            f'energies = {energies.min()}, {energies.mean()}, {energies.max()}'
+        )
+
         derivatives = 6.0 * epsilon_x4 * (
                 2.0 * sigma_pow6 * sigma_pow6 / (self.distances ** 13)
                 - sigma_pow6 / (self.distances ** 7)
